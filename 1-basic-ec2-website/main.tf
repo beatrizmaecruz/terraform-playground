d5a4439fb799e060d3440009f145e4687f88b039
Beatriz Cruz <beatrizmaecruz@proton.me>
2026-01-19 09:58:06 +1300

    Attempted fix for basic-ec2-website

--- 1-basic-ec2-website/main.tf
+++ 1-basic-ec2-website/main.tf
@@ -13,7 +13,7 @@ resource "aws_vpc_security_group_egress_rule" "egress" {
   ip_protocol = "-1"
 }
 
-resource "aws_vpc_security_group_ingress_rule" "ingress" {
+resource "aws_vpc_security_group_ingress_rule" "ingress_http" {
   security_group_id = aws_security_group.sg.id
   cidr_ipv4   = "0.0.0.0/0"
   from_port   = 80
@@ -21,11 +21,33 @@ resource "aws_vpc_security_group_ingress_rule" "ingress" {
   to_port     = 80
 }
 
+resource "aws_vpc_security_group_ingress_rule" "ingress_https" {
+  security_group_id = aws_security_group.sg.id
+  cidr_ipv4   = "0.0.0.0/0"
+  from_port   = 443
+  ip_protocol = "tcp"
+  to_port     = 443
+}
+
+resource "aws_vpc_security_group_ingress_rule" "ingress_ssh" {
+  security_group_id = aws_security_group.sg.id
+  cidr_ipv4   = "0.0.0.0/0"
+  from_port   = 22
+  ip_protocol = "tcp"
+  to_port     = 22
+}
+
+
+resource "aws_key_pair" "key_pair" {
+  key_name   = "deployer-key"
+  public_key = file("~/.ssh/mykey.pub")
+}
+
 resource "aws_instance" "web" {
-  ami           = "ami-0b3c832b6b7289e44"  # Change the AMI accordingly
+  ami           = "ami-0ef6d0055be7553ee"  # AMI for Debian (compatible with apt)
   instance_type = "t3.micro"
   vpc_security_group_ids = [aws_security_group.sg.id]
-
+  key_name      = aws_key_pair.key_pair.key_name
   user_data     = file("userdata.sh")
 
   tags = {
